916296d8c206a21697ad4852c3849149e1e80bf3
mrkhalid000 <kingcoding11@gmail.com>
2024-10-02 15:54:00 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,9 +9,9 @@ global.socialm = "GitHub: EMMYHENZ-TECH" //ur github or insta name
 global.location = "Africa, Nigeria, calabar" //ur location
 
 //new
-global.botname = '© ERROR-V1' //ur bot name
-global.ownernumber = ['2349125042727'] //ur owner number, dont add more than one
-global.ownername = '© EMMYHENZ' //ur owner name
+global.botname = '© ᏦᏆᑎᏀ ᏦᕼᗩしᏆᗞ' //ur bot name
+global.ownernumber = ['2348023898600'] //ur owner number, dont add more than one
+global.ownername = '© ᏦᏆᑎᏀ ᏦᕼᗩしᏆᗞ' //ur owner name
 global.websitex = "https://www.youtube.com/@EMMYHENZTECHINFO"
 global.wagc = "https://whatsapp.com/channel/0029VangYOt96H4JhFarL10C"
 global.themeemoji = '🪀'
@@ -19,9 +19,9 @@ global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/' //script link
 global.packname = "Sticker By"
 global.author = "EMMYTECHINFO\n\n+2349121785627"
-global.creator = "2349125042727@s.whatsapp.net"
+global.creator = "2348023898600@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["2349125042727"] // Premium User
+global.premium = ["2348023898600"] // Premium User
 
 //theme link
 global.link = 'https://whatsapp.com/channel/0029VangYOt96H4JhFarL10C'
